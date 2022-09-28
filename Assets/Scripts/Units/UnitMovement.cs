0649b84bcdf0fd087dbce7f62a141aa0fdc00ee4
Ismayil Hasanov <ismailhasanov@gmail.com>
2022-09-28 15:00:24 +0300

    feat: unit spawner and unit movement added

---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,47 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class UnitMovement : NetworkBehaviour
+{
+    #region Variables
+    [SerializeField] private NavMeshAgent agent = null;
+    private Camera mainCamera;
+    #endregion
+
+    #region Server
+
+    [Command]
+    private void CmdMove(Vector3 position)
+    {
+        if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
+
+        agent.SetDestination(hit.position);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        mainCamera = Camera.main;
+    }
+
+    [ClientCallback]
+    private void Update()
+    {
+        if (!hasAuthority) { return; }
+
+        if (!Mouse.current.rightButton.wasPressedThisFrame) { return; }
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
+
+        CmdMove(hit.point);
+    }
+
+    #endregion
+}
