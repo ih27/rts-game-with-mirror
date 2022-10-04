b5d14cb4703d0054d217f2899d683280ae9dac78
Ismayil Hasanov <ismailhasanov@gmail.com>
2022-10-04 17:31:11 +0300

    feat: selecting and highlighting units added

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,29 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!hasAuthority) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!hasAuthority) { return; }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelectionHandler : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask = new LayerMask();
+    private Camera mainCamera;
+    private List<Unit> selectedUnits = new List<Unit>();
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            // TODO: Start selection area
+            StartSelectionArea();
+
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            ClearSelectionArea();
+        }
+    }
+
+    private void StartSelectionArea()
+    {
+        foreach (Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Deselect();
+        }
+
+        selectedUnits.Clear();
+    }
+
+    private void ClearSelectionArea()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
+
+        if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
+
+        if (!unit.hasAuthority) { return; }
+
+        selectedUnits.Add(unit);
+
+        foreach (Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Select();
+        }
+    }
+}
