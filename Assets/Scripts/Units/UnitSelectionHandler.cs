79acf750a27863eeb63a44f46974c2cfc4b04949
Ismayil Hasanov <ismailhasanov@gmail.com>
2022-10-17 12:57:15 +0300

    feat: multi-select implemented

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class UnitSelectionHandler : MonoBehaviour
 {
+    [SerializeField] private RectTransform unitSelectionArea = null;
+
     [SerializeField] private LayerMask layerMask = new LayerMask();
+
+    private Vector2 startPosition;
+
+    private RTSPlayer player;
     private Camera mainCamera;
     public List<Unit> SelectedUnits { get; } = new List<Unit>();
 
@@ -17,15 +24,23 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private void Update()
     {
+        // TODO: temp fix till Lobby implementation
+        if (player == null)
+        {
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            // TODO: Start selection area
             StartSelectionArea();
-
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             ClearSelectionArea();
+        }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            UpdateSelectionArea();
         }
     }
 
@@ -37,23 +52,68 @@ public class UnitSelectionHandler : MonoBehaviour
         }
 
         SelectedUnits.Clear();
+
+        unitSelectionArea.gameObject.SetActive(true);
+        startPosition = Mouse.current.position.ReadValue();
+
+        // This needs to be called on the first frame when left button is clicked
+        UpdateSelectionArea();
+
     }
 
     private void ClearSelectionArea()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        unitSelectionArea.gameObject.SetActive(false);
 
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
-
-        if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
-
-        if (!unit.hasAuthority) { return; }
-
-        SelectedUnits.Add(unit);
-
-        foreach (Unit selectedUnit in SelectedUnits)
+        if (unitSelectionArea.sizeDelta.magnitude == 0)
         {
-            selectedUnit.Select();
+            // Single click
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
+
+            if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
+
+            if (!unit.hasAuthority) { return; }
+
+            SelectedUnits.Add(unit);
+
+            foreach (Unit selectedUnit in SelectedUnits)
+            {
+                selectedUnit.Select();
+            }
+
+            return;
+        }
+
+        // Multiselect
+        Vector2 min = unitSelectionArea.anchoredPosition - (unitSelectionArea.sizeDelta / 2);
+        Vector2 max = unitSelectionArea.anchoredPosition + (unitSelectionArea.sizeDelta / 2);
+
+        foreach (Unit unit in player.GetMyUnits())
+        {
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position);
+
+            if (screenPosition.x > min.x &&
+                   screenPosition.x < max.x &&
+                   screenPosition.y > min.y &&
+                   screenPosition.y < max.y)
+            {
+                SelectedUnits.Add(unit);
+                unit.Select();
+            }
         }
     }
+
+    private void UpdateSelectionArea()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        float areaWidth = mousePosition.x - startPosition.x;
+        float areaHeight = mousePosition.y - startPosition.y;
+
+        unitSelectionArea.sizeDelta = new Vector2(Mathf.Abs(areaWidth), Mathf.Abs(areaHeight));
+        unitSelectionArea.anchoredPosition = startPosition + new Vector2(areaWidth / 2, areaHeight / 2);
+    }
+
 }
