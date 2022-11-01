5734a85a8d377e9aa4f2c00242f10328aa544f82
Ismayil Hasanov <ismailhasanov@gmail.com>
2022-11-01 12:44:57 +0300

    feat: dealing damage and health script added

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    #region Variables
+
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar]
+    private int currentHealth;
+
+    public event Action ServerOnDie;
+    #endregion
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        currentHealth = maxHealth;
+    }
+
+    [Server]
+    public void DealDamage(int damageAmount)
+    {
+        if (currentHealth == 0) { return; }
+
+        currentHealth = Mathf.Max(currentHealth - damageAmount, 0);
+
+        if (currentHealth != 0) { return; }
+
+        // dead
+        ServerOnDie?.Invoke();
+
+        Debug.Log("We died!");
+    }
+
+    #endregion
+
+    #region Client
+
+
+    #endregion
+}
--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -8,6 +8,7 @@ public class UnitProjectile : NetworkBehaviour
     #region Variables
 
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -21,6 +22,22 @@ public class UnitProjectile : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) { return; }
+        }
+
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
     }
 
     #region Server
